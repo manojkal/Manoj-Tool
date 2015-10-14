19b943185e5f5efc1b16485ffba54ea1492f8267
manoj <manoj@manoj>
2015-10-14 15:08:54 -0500

    intial commit of my factors

---
+++ Assignments/src/com/manoj/org/Factor.java
@@ -0,0 +1,11 @@
+package com.manoj.org;
+
+public class Factor {
+public static void main(String[] args) {
+	int ans = 1;
+    for (int i = 1; i <= 5; i++) {
+        ans = ans * i;
+    }
+    System.out.println("The factorial of 5 is " + ans);
+}
+}
